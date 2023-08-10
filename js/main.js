8ba68099612fb1f4ec38d4940910d2f42f97cc53
Muhammadumar177 <bekumar1107@gmail.com>
2023-08-10 13:06:42 +0500

    toggle dark-theme  onsite-header theme-toglger click

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const elSiteThmeToggler = document.querySelector(".site-header-theme-toggler");
+elSiteThmeToggler.addEventListener("click", function(){
+
+  document.body.classList.toggle("dark-mode");
+
+
+})
